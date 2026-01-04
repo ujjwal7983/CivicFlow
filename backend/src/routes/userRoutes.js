3512885f7320f4e8c2799895880716a0f17c3917
Samyak <samyaknigam.11@gmail.com>
2026-01-04 21:48:57 +0530

    Data from backend fixed

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,24 @@
 import express from "express";
 import protect from "../middlewares/authMiddleware.js"
 import authorize from "../middlewares/roleMiddleware.js"
+import User from "../models/User.js"
 
 const router = express.Router();
 
 router.get("/home",protect,authorize("CITIZEN"),
-    (req, res) => {
-        res.status(200).json({message: "Welcome to Citizen Home",user: req.user});
+    async (req, res) => {
+        try {
+      const user = await User.findById(req.user.id).select(
+        "name email role"
+      );
+
+      res.status(200).json({
+        message: "Welcome to Citizen Home",
+        user,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
     }
 );
 

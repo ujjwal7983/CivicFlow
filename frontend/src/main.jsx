bf14f0da58bf3727ec113970995adf575b1734c7
Ujjwal Soni <ujjwalsoni7983@gmail.com>
2026-01-13 20:19:28 +0530

    Fix AuthContext path casing for Linux build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './context/AuthContext.jsx'
+import AuthContext from './Context/AuthContext.jsx'
 import UserContext from './Context/UserContext.jsx'
 
 
